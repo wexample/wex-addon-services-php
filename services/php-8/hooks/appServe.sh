d7a66c726627f1d526683d7ec5e5d35e4b666761
weeger <romain.weeger@gmail.com>
2023-01-20 17:12:09 +0100

    app serve on start

--- services/php-8/hooks/appServe.sh
+++ services/php-8/hooks/appServe.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 php8AppServe() {
-  wex app::app/exec -c="service php8.2-fpm restart" -i
+  wex app::app/exec -c="service php8.2-fpm start" -i
   wex app::app/exec -c="service apache2 restart" -i
 }
--- services/php-8/hooks/appStarted.sh
+++ services/php-8/hooks/appStarted.sh
@@ -4,4 +4,6 @@ php8AppStarted() {
   # Wait mounted volumes to be available
   _wexLog "Waiting for apache to restart..."
   sleep 5
+
+  wex app::app/serve
 }
