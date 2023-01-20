2953b97d4ad098dddaf1e4c508c4cb334dfbf0d1
weeger <romain.weeger@gmail.com>
2023-01-20 16:11:04 +0100

    Improve starting

--- services/php-8/hooks/appServe.sh
+++ services/php-8/hooks/appServe.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 php8AppServe() {
-  wex app::app/exec -c="service apache2 restart"
+  wex app::app/exec -c="service php8.2-fpm restart" -i
+  wex app::app/exec -c="service apache2 restart" -i
 }
