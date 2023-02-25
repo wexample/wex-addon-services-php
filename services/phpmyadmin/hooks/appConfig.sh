edd166948f5455f70850ed46b39b92c1350498d1
weeger <romain.weeger@gmail.com>
2023-02-25 06:56:10 +0100

    PhpMyAdmin config

--- services/phpmyadmin/hooks/appConfig.sh
+++ services/phpmyadmin/hooks/appConfig.sh
@@ -2,19 +2,4 @@
 
 phpmyadminAppConfig() {
   wex-exec app::config/bindFiles -s=php -e=ini
-
-  . "${WEX_FILEPATH_REL_CONFIG}"
-
-  local DOMAIN
-  DOMAIN=$(eval 'echo ${'"${APP_ENV^^}"'_DOMAIN_PMA}')
-
-  if [ "${APP_ENV}" = "local" ]; then
-    if [ "${DOMAIN_MAIN}" = '' ]; then
-      DOMAIN=pma.${NAME}.wex
-    else
-      DOMAIN=pma.${DOMAIN_MAIN}
-    fi
-  fi
-
-  wex-exec app::config/setValue -k=DOMAIN_PMA -v="${DOMAIN}"
 }
