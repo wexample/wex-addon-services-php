9a08bceb584f979e26ceb17fcf1df1018b2b35bf
weeger <romain.weeger@gmail.com>
2023-03-01 14:00:33 +0100

    Fix tests

--- services/laravel-5/hooks/serviceInstall.sh
+++ services/laravel-5/hooks/serviceInstall.sh
@@ -3,7 +3,7 @@
 laravel5ServiceInstall() {
   wex-exec app::config/setValue -b -k=MAIN_CONTAINER_NAME -v="laravel_5"
 
-  wex-exec service/install -i -s=php-8 -g=${1} \
+  wex-exec service/install -s=php-8 -g=${1} \
     -ic=false \
     -id=false
 }
--- services/symfony-5/hooks/serviceInstall.sh
+++ services/symfony-5/hooks/serviceInstall.sh
@@ -3,7 +3,7 @@
 symfony5ServiceInstall() {
   wex-exec app::config/setValue -b -k=MAIN_CONTAINER_NAME -v=symfony_5
 
-  wex-exec service/install -i -s=php-8 -g=${1} \
+  wex-exec service/install -s=php-8 -g=${1} \
     -ic=false \
     -id=false
 }
