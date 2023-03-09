1f29b32cbe30fd62a6d6f0de9bbfec0e9d08fc5c
weeger <romain.weeger@gmail.com>
2023-03-09 20:08:11 +0100

    Fix PMA domain

--- services/phpmyadmin/hooks/appConfig.sh
+++ services/phpmyadmin/hooks/appConfig.sh
@@ -2,4 +2,22 @@
 
 phpmyadminAppConfig() {
   wex-exec app::config/bindFiles -s=php -e=ini
+
+  local DOMAIN_PMA
+  . "${WEX_FILEPATH_REL_CONFIG}"
+
+  if [ "${DOMAIN_PMA}" = "" ]; then
+    local DOMAIN
+    DOMAIN=$(eval 'echo ${'"${APP_ENV^^}"'_DOMAIN_PMA}')
+
+    if [ "${APP_ENV}" = "local" ]; then
+      if [ "${DOMAIN_MAIN}" = '' ]; then
+        DOMAIN=pma.${NAME}.wex
+      else
+        DOMAIN=pma.${DOMAIN_MAIN}
+      fi
+    fi
+
+    wex-exec app::config/setValue -k=DOMAIN_PMA -v="${DOMAIN}"
+  fi
 }
